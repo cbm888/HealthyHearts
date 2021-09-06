77a1706b6b0fa2f55e8d2ba6fbf4bcd576eba6e1
cbm888 <cbmarch88@gmail.com>
2021-09-05 20:54:19 -0500

    Add files via upload

---
+++ Healthy Hearts/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Healthy_Hearts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+
+        }
+
+
+        public void HeartRate()
+        {
+            Console.WriteLine("What is your heart rate?");
+            string userInput = Console.ReadLine();
+            int userAge = Int32.Parse(userInput);
+
+
+        }
+
+
+    }
+}
